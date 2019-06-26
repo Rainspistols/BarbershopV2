4a5eddeac14ca7d2701f96fc792239bfadad5d52
Rainspistols <ffly@mail.ru>
2019-06-26 21:42:53 +0200

    added rubbery for tablet

---
+++ app/js/advantages-slider.js
@@ -0,0 +1,7 @@
+var quickBtn=document.querySelector(".slider__toggle1");
+var coolBtn=document.querySelector(".slider__toggle2");
+var expensiveBtn=document.querySelector("slider__toggle3");
+var quickSlide=document.querySelector(".advantages__item--quick");
+var coolSlide=document.querySelector(".advantages__item--cool");
+var expensiveSlide=document.querySelector(".advantages__item--expensive");
+
