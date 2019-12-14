be1782ccceb1de4b5b0de1e57fb3514e25cd6171
Rainspistols <ffly@mail.ru>
2019-12-14 14:47:25 +0100

    finished all pages update

--- src/js/news.js
+++ src/js/news.js
@@ -1,10 +1,15 @@
-let showBtn = document.querySelector(".news__button"),
-  newsBlock = document.querySelector(".news__item--mobile3");
+if (
+  document.querySelector('.news__button') ||
+  document.querySelector('.news__item--mobile3')
+) {
+  let showBtn = document.querySelector('.news__button'),
+    newsBlock = document.querySelector('.news__item--mobile3');
 
-showBtn.addEventListener("click", function() {
-  if (!newsBlock.classList.contains("js-show")) {
-    newsBlock.classList.add("js-show");
-  } else {
-    newsBlock.classList.remove("js-show");
-  }
-})
+  showBtn.addEventListener('click', function() {
+    if (!newsBlock.classList.contains('js-show')) {
+      newsBlock.classList.add('js-show');
+    } else {
+      newsBlock.classList.remove('js-show');
+    }
+  });
+}
